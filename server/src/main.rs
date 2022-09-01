1d4cb104cd4a68be83840d2d860c21cd600e7a34
Q4LEX <q4lex44@gmail.com>
2022-09-01 02:46:14 +0200

    stuffs

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,12 +6,29 @@ async fn index() -> impl Responder {
     NamedFile::open("./static/html/index.html")
 }
 
+#[get("/alien_packs")]
+async fn alien_packs() -> impl Responder {
+    NamedFile::open("./static/html/alien_packs.html")
+}
+
+#[get("/coming_soon")]
+async fn coming_soon() -> impl Responder {
+    NamedFile::open("./static/html/coming_soon.html")
+}
+
+async fn error_404() -> impl Responder {
+    NamedFile::open("./static/html/error_404.html")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(actix_files::Files::new("/static", "./static").show_files_listing().prefer_utf8(true))
             .service(index)
+            .service(alien_packs)
+            .service(coming_soon)
+            .default_service(web::route().to(error_404))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- server/static/js/base.js
+++ server/static/js/base.js
@@ -2,19 +2,33 @@ function toggle_mc_dropdown() {
     
 }
 
-var is_menu_shown = false;
+var is_menu_shown_mc = false;
+var is_menu_shown_p = false;
 $(document).ready(function () {
-    let btn = $("#navbar_item_minecraft_text");
-    let menu = $("#navbar_mc_dropdown_menu");
+    let btn_mc = $("#navbar_item_minecraft_text");
+    let menu_mc = $("#navbar_mc_dropdown_menu");
 
-    btn.click(function () {
-        if (is_menu_shown) {
-            menu.hide();
+    btn_mc.click(function () {
+        if (is_menu_shown_mc) {
+            menu_mc.hide();
         } else {
-            menu.show();
-            menu.css("display", "flex");
+            menu_mc.show();
+            menu_mc.css("display", "flex");
         }
-        is_menu_shown = !is_menu_shown;
+        is_menu_shown_mc = !is_menu_shown_mc;
+    });
+
+    let btn_p = $("#navbar_item_partners_text");
+    let menu_p = $("#navbar_partners_dropdown_menu");
+
+    btn_p.click(function () {
+        if (is_menu_shown_p) {
+            menu_p.hide();
+        } else {
+            menu_p.show();
+            menu_p.css("display", "flex");
+        }
+        is_menu_shown_p = !is_menu_shown_p;
     });
 
     $("#navbar_alien_logo img").hover(function() {
@@ -22,5 +36,40 @@ $(document).ready(function () {
         
     }, function() {
         $(this).attr("src", "/static/resources/icons/mr3dalien_white.png.webp");
+    });
+
+    $("#navbar_social_youtube img").hover(function() {
+        $(this).attr("src", "/static/resources/icons/youtube_button_color.webp");
+        
+    }, function() {
+        $(this).attr("src", "/static/resources/icons/youtube_button.webp");
+    });
+
+    $("#navbar_social_twitter img").hover(function() {
+        $(this).attr("src", "/static/resources/icons/twitter_button_color.webp");
+        
+    }, function() {
+        $(this).attr("src", "/static/resources/icons/twitter_button.webp");
+    });
+
+    $("#navbar_social_patreon img").hover(function() {
+        $(this).attr("src", "/static/resources/icons/patreon_button_color.webp");
+        
+    }, function() {
+        $(this).attr("src", "/static/resources/icons/patreon_button.webp");
+    });
+
+    $("#navbar_social_planetminecraft img").hover(function() {
+        $(this).attr("src", "/static/resources/icons/planetminecraft_button_color.webp");
+        
+    }, function() {
+        $(this).attr("src", "/static/resources/icons/planetminecraft_button.webp");
+    });
+
+    $("#navbar_social_discord img").hover(function() {
+        $(this).attr("src", "/static/resources/icons/discord_button_color.webp");
+        
+    }, function() {
+        $(this).attr("src", "/static/resources/icons/discord_button.webp");
     });
 });
